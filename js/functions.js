75d3c63990b56ce8764684a2ee7146ef90e85725
stuartshi <stuart.shi@icloud.com>
2017-09-18 10:26:12 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -110,7 +110,7 @@ function timeElapse(c) {
 }
 function showMessages() {
     adjustWordsPosition();
-    $("#messages").fadeIn(5000, function() {
+    $("#messages").fadeIn(10, function() {
         showLoveU()
     })
 }
